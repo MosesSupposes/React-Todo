20d2f37dd2944564b5c9baccbcd17cd705fc0caf
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-02 11:24:46 -0700

    add dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        task: "Learn Vim",
+        id: Date.now(),
+        completed: false
+    },
+    {
+        task: "Learn Python",
+        id: Date.now(),
+        completed: false
+    },
+    {
+        task: "Improve at Elm",
+        id: Date.now(),
+        completed: false
+    }
+]
