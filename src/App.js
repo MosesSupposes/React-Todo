61cfd3ae8e261764075ed8c23ea0df83cfc6640b
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-02 12:10:50 -0700

    add and render a working Todo form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import dummyData from './dummyData'
 import TodoList from './components/TodoComponents/TodoList'
+import TodoForm from './components/TodoComponents/TodoForm'
 
 class App extends React.Component {
   state = {
-    todos: dummyData
+    todos: dummyData,
   }
+
+  addTodo = todo => {
+    this.setState(prevState => 
+      ({ todos: prevState.todos.concat(todo) })
+    )
+  }
+  
   render() {
     return (
       <div>
+        <TodoForm addTodo={this.addTodo} />
         <TodoList todos={this.state.todos} />
       </div>
     )
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import uuid from 'uuid'
+
+export default class TodoForm extends Component {
+    state = {
+        currentTodo: ''
+    }
+    
+    handleChange = event => {
+        this.setState({ currentTodo: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.props.addTodo({
+            id: uuid(),
+            task: this.state.currentTodo,
+            completed: false
+        })
+        this.setState({currentTodo: ''})
+    }
+    
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    type="text"
+                    onChange={this.handleChange}
+                    value={this.state.currentTodo}
+                    placeholder="enter a task"
+                />
+                <button type="submit">+</button>
+                <button>Clear</button>
+            </form>
+    )
+    }
+}
